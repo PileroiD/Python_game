f2c6dab480300bf8cae82b9b370961173a67408d
Sasha <example@gmail.com>
2021-01-11 17:24:16 +0200

    'Password_mod'

---
+++ Password/generation_password.py
@@ -0,0 +1,23 @@
+import random
+
+sumbols = '123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM' \
+          '!@#$%^&*()_+|-=\\/<>/?:;"\'{}[]~'
+
+
+def password2(sumbol: str, lens: int):
+    while True:
+        result = []
+        for _ in range(lens):
+            result.append(random.choice(list(sumbol)))
+        # print(''.join(result))
+        result = ''.join(result)
+        print(f'\n{result}\n')
+        q = input('Again?[y/n]')
+        if q == 'y':
+            continue
+        else:
+            break
+
+
+n = int(input("Введите количество символов в пароле:"))
+password2(sumbols, n)
--- generation_password.py
+++ generation_password.py
@@ -4,11 +4,20 @@ sumbols = '123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM' \
           '!@#$%^&*()_+|-=\\/<>/?:;"\'{}[]~'
 
 
-def password(sumbol, ln):
-    result = []
-    for _ in range(ln):
-        result.append(random.choice(list(sumbol)))
-    return ''.join(result)
+def password2(sumbol: str, lens: int):
+    while True:
+        result = []
+        for _ in range(lens):
+            result.append(random.choice(list(sumbol)))
+        # print(''.join(result))
+        result = ''.join(result)
+        print(f'\n{result}\n')
+        q = input('Again?[y/n]')
+        if q == 'y':
+            continue
+        else:
+            break
 
 
-print(password(sumbols, 10))
+n = int(input("Введите количество символов в пароле:"))
+password2(sumbols, n)
